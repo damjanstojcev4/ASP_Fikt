e00ae880b630a1356f5be56ad74fe891971e87be
Damjan Stojchev <damjan.stojcev4@gmail.com>
2023-11-01 18:15:31 +0100

    Binary Search Algorithm

---
+++ src/search/linear_search/BinarySearch.java
@@ -0,0 +1,37 @@
+package search.linear_search;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target element found at index mid
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Target is in the right half
+            } else {
+                right = mid - 1; // Target is in the left half
+            }
+        }
+
+        return -1; // Target element not found in the array
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 11, 12, 13};
+        int target = 13;
+
+        int result = binarySearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+}
+
